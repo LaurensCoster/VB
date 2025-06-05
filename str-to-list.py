670f06096987b7cb8bd255b3658adafccaf3b465
BartoszPawlaklc <bartosz.pawlak@laurenscoster.com>
2025-06-05 10:22:18 +0200

    Update str-to-list.py

--- str-to-list.py
+++ str-to-list.py
@@ -1,7 +1,6 @@
 import functions_framework
 import json
 
-
 @functions_framework.http
 def hello_http(request):
     req = request.get_json(silent=True)
@@ -9,10 +8,16 @@ def hello_http(request):
     session_params = req.get('sessionInfo', {}).get('parameters', {})
     lista = session_params.get(tag)
     print(repr(lista))
-    lista = json.loads(lista)
-
+    try:
+        lista = json.loads(lista)
+    except:
+        try:
+            lista = ast.literal_eval(lista)
+        except:
+            lista = []
+    print(lista)
     session_params[tag] = lista
-
+    
     response = {
         "sessionInfo": {
             "parameters": session_params
